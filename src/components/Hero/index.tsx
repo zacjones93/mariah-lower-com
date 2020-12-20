5460ab6d975dc01ca3f31099569ac0f9c7a3529c
Zac Jones <zacjones93@gmail.com>
2020-12-19 23:30:27 -0500

    Update hero component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,7 +3,7 @@ export const Hero = () => {
     <main className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 h-96">
       <div className="text-center">
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">Get healthy with </span>
+          <span className="block xl:inline">Get healthy through </span>
           <span className="block text-green-500 xl:inline">
             sustainable habits
           </span>
