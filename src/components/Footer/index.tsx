8aee3d6aadb714c2835492d25c506c0dfcad98d7
Zac Jones <zacjones93@gmail.com>
2020-12-19 23:29:52 -0500

    update background color

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="bg-white">
+    <footer className="bg-gray-50">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav
           className="-mx-5 -my-2 flex flex-wrap justify-center"
