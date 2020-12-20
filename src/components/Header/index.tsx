edbad05ac30f369e0d97307d84c69cfffc385927
Zac Jones <zacjones93@gmail.com>
2020-12-20 14:58:15 -0800

    nav item alignment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,17 +18,17 @@ export const Header = () => {
       <div className="mt-4 pb-2 flex md:mt-0 md:ml-4 justify-center">
         <NextLink href="/about">
           <a
-            className="ml-3 sm:text-base inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium
+            className="ml-3 sm:text-base inline-flex items-center px-4 mt-1 pt-3 pb-2 border border-transparent rounded-md shadow-sm text-sm font-medium
          text-gray-900  hover:bg-green-500    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             About
           </a>
         </NextLink>
-        <a className="ml-3 sm:text-base inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+        <a className="ml-3 sm:text-base inline-flex items-center px-4 mt-1 pt-3 pb-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           Training
         </a>
         <a
-          className="ml-3 mr-3 sm:text-base inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 hover:bg-green-500
+          className="ml-3 mr-3 sm:text-base inline-flex items-center px-4 mt-1 pt-3 pb-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900 hover:bg-green-500
          focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Nutrition
