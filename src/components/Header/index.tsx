ec1069c086071f85816789d2c594ec2ac6f92f0e
Zac Jones <zacjones93@gmail.com>
2020-12-20 14:55:50 -0800

    Tweak header style alignment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import NextLink from 'next/link'
 
 export const Header = () => {
   return (
-    <div className="mt-3 md:flex md:items-center md:justify-between bg-gray-600">
+    <div className="md:flex md:items-center md:justify-between bg-gray-600">
       <div className="flex min-w-0">
         <img
           className="inline-block h-14 w-15 "
@@ -10,14 +10,17 @@ export const Header = () => {
           alt="kettlebell drawing"
         />
         <NextLink href="/">
-          <h2 className="ml-3 text-2xl font-bold leading-7 pt-4 text-green-500 sm:text-4xl sm:truncate cursor-pointer">
+          <h2 className="ml-3 items-baseline font-bold leading-7 pt-7 sm:pt-6 text-green-500 text-4xl sm:truncate cursor-pointer align-bottom">
             Mariah Lower
           </h2>
         </NextLink>
       </div>
-      <div className="mt-4 flex md:mt-0 md:ml-4">
+      <div className="mt-4 pb-2 flex md:mt-0 md:ml-4 justify-center">
         <NextLink href="/about">
-          <a className="ml-3 sm:text-base inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-900  hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <a
+            className="ml-3 sm:text-base inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium
+         text-gray-900  hover:bg-green-500    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
             About
           </a>
         </NextLink>
