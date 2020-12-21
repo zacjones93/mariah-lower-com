f0faeb18fd9980cdaa503a64f3fa178b55be6486
Zac Jones <zacjones93@gmail.com>
2020-12-20 16:51:04 -0800

    Add mailchimp integration

--- src/components/Email/index.tsx
+++ src/components/Email/index.tsx
@@ -1,4 +1,14 @@
+import {useState, createRef} from 'react'
+import MailchimpSubscribe from 'react-mailchimp-subscribe'
+
+const signUpURL =
+  'https://mariahlower.us7.list-manage.com/subscribe/post?u=9069f120e7988f441e57a2f3a&amp;id=38cf29bbdc'
+
 export const Email: any = () => {
+  const emailRef = createRef(undefined)
+  const nameRef = createRef(undefined)
+  const messageRef = createRef(undefined)
+
   return (
     <div className="bg-white min-w-max max-w-min mx-auto">
       <section className="py-12 sm:px-4 lg:py-16 grid-cols-1">
@@ -18,51 +28,88 @@ export const Email: any = () => {
             I'm open to talking any time
           </p>
         </div>
-        <form className="mt-8 py-2 w-screen max-w-sm sm:max-w-none sm:w-1/2 sm:mx-auto bg-gray-500 rounded-md">
-          <div className="justify-center">
-            <label htmlFor="name" className="sr-only">
-              First Name
-            </label>
-            <input
-              id="name"
-              name="name"
-              required
-              className="mx-7 w-10/12 px-3 py-3 mt-2 justify-center   placeholder-gray-500 focus:ring-indigo-500 focus:text-green-500
-             border-gray-300 rounded-md"
-              placeholder="Enter your name"
-            />
+        <MailchimpSubscribe
+          url={signUpURL}
+          render={({subscribe, status, message}) => {
+            switch (status) {
+              case 'sending':
+                return <div>Sending...</div>
+              case 'success':
+                return <div>Subscribed.</div>
+              case 'error':
+                return <div dangerouslySetInnerHTML={{__html: message}} />
+              default:
+                return (
+                  <form
+                    onSubmit={() => {
+                      event.preventDefault()
+                      alert(
+                        JSON.stringify({
+                          EMAIL: emailRef.current.value,
+                          NAME: nameRef.current.value,
+                          MESSAGE: messageRef.current.value,
+                        }),
+                      )
+                      subscribe({
+                        EMAIL: emailRef.current.value,
+                        NAME: nameRef.current.value,
+                        MESSAGE: messageRef.current.value,
+                      })
+                    }}
+                    className="mt-8 py-2 w-screen max-w-sm sm:max-w-none sm:w-1/2 sm:mx-auto bg-gray-500 rounded-md"
+                  >
+                    <div className="justify-center">
+                      <label htmlFor="name" className="sr-only">
+                        First Name
+                      </label>
+                      <input
+                        id="name"
+                        name="name"
+                        required
+                        ref={nameRef}
+                        className="mx-7 w-10/12 px-3 py-3 mt-2 justify-center   placeholder-gray-500 focus:ring-indigo-500 focus:text-green-500
+              border-gray-300 rounded-md"
+                        placeholder="Enter your name"
+                      />
 
-            <label htmlFor="emailAddress" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="emailAddress"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="mx-7 w-10/12 px-3 py-3 mt-2 justify-center  placeholder-gray-500 focus:ring-indigo-500 focus:text-green-500 max-w-xs border-gray-300 rounded-md"
-              placeholder="Enter your email"
-            />
+                      <label htmlFor="emailAddress" className="sr-only">
+                        Email address
+                      </label>
+                      <input
+                        id="emailAddress"
+                        name="email"
+                        type="email"
+                        ref={emailRef}
+                        autoComplete="email"
+                        required
+                        className="mx-7 w-10/12 px-3 py-3 mt-2 justify-center  placeholder-gray-500 focus:ring-indigo-500 focus:text-green-500 max-w-xs border-gray-300 rounded-md"
+                        placeholder="Enter your email"
+                      />
 
-            <label htmlFor="message" className="sr-only">
-              Message
-            </label>
-            <textarea
-              id="message"
-              name="message"
-              className="mx-7 w-10/12 justify-center px-5 py-3 mt-2 placeholder-gray-500 focus:ring-indigo-500 focus:text-green-500 max-w-xs border-gray-300 rounded-md"
-              placeholder="Say hi!"
-            />
+                      <label htmlFor="message" className="sr-only">
+                        Message
+                      </label>
+                      <textarea
+                        id="message"
+                        name="message"
+                        ref={messageRef}
+                        className="mx-7 w-10/12 justify-center px-5 py-3 mt-2 placeholder-gray-500 focus:ring-indigo-500 focus:text-green-500 max-w-xs border-gray-300 rounded-md"
+                        placeholder="Say hi!"
+                      />
 
-            <button
-              type="submit"
-              className="w-40 mx-auto flex items-center justify-center my-3 px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:text-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Send
-            </button>
-          </div>
-        </form>
+                      <button
+                        type="submit"
+                        value="subscribe"
+                        className="w-40 mx-auto flex items-center justify-center my-3 px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:text-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        Send
+                      </button>
+                    </div>
+                  </form>
+                )
+            }
+          }}
+        />
       </section>
     </div>
   )
